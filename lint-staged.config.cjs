0752d671644e824eccba6f8cfcb95a09e381eaa0
jv-vogler <jvsvogler@gmail.com>
2023-11-10 17:06:19 -0300

    chore: fix lint-staged config

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,14 +1,10 @@
 module.exports = {
-  // Type check TypeScript files that are staged
-  '**/*.(ts|tsx)': (filenames) =>
-    filenames.map((filename) => `npx tsc --noEmit ${filename} --jsx react-jsx`),
+  // Type check
+  '**/*.{ts,tsx}': [() => 'tsc -p tsconfig.json --skipLibCheck --noEmit'],
 
-  // Lint then format TypeScript and JavaScript files
-  '**/*.(ts|tsx|js|jsx)': (filenames) => [
-    `npx eslint --fix ${filenames.join(' ')}`,
-    `npx prettier --write ${filenames.join(' ')}`,
-  ],
+  // Lint
+  '**/*.{ts,tsx,js,jsx}': 'eslint --fix',
 
-  // Format MarkDown and JSON
-  '**/*.(md|json)': (filenames) => `npx prettier --write ${filenames.join(' ')}`,
+  // Format
+  '**/*.{ts,tsx,js,jsx,md,json,html,css}': 'prettier --write',
 }
