059cfd13313d215e16a126be6eb3fa10d7537ac8
jv-vogler <jvsvogler@gmail.com>
2023-11-11 13:47:22 -0300

    feat: create redux store file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
+import { Provider } from 'react-redux'
+
 import type { AppProps } from 'next/app'
 
+import { store } from '@/state/store'
+import '@/styles/globals.css'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
